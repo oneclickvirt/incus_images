ccbfe1a8426c79f0d886ba29999c6e7f9d201972
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-01-13 11:23:34 +0800

    Update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 
+cd /root >/dev/null 2>&1
 snap install distrobuilder --classic
 apt-get install debootstrap -y
-
-
+rm -r images
+mkdir images
+cd ./images
 debian_versions=("stretch" "buster" "bullseye" "bookworm" "trixie")
 architectures=("amd64" "arm64" "armel" "armhf" "i386" "ppc64el" "s390x")
 variants=("default", "cloud")
+mkdir debian
+cd ./debian
 for version in "${debian_versions[@]}"; do
     for arch in "${architectures[@]}"; do
         for variant in "${variants[@]}"; do
-            distrobuilder build-lxc debian.yaml -o image.release=${version} -o image.architecture=${arch} -o image.variant=${variant}
-            mv meta.tar.xz
-            mv rootfs.tar.xz
+            mkdir ${version}
+            cd ./${version}
+            mkdir ${arch}
+            cd ./${arch}
+            mkdir ${variant}
+            cd ./${variant}
+            distrobuilder build-lxc /root/images_yaml/debian.yaml -o image.release=${version} -o image.architecture=${arch} -o image.variant=${variant}
+            cd ..
+            cd ..
+            cd ..
         done
     done
 done

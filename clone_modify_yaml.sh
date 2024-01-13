22dcec0a212f031121b31779599a471c76a3311a
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-01-13 17:28:15 +0800

    Update clone_modify_yaml.sh

--- clone_modify_yaml.sh
+++ clone_modify_yaml.sh
@@ -5,4 +5,4 @@ wget https://raw.githubusercontent.com/lxc/lxc-ci/main/images/debian.yaml
 chmod 777 debian.yaml
 sed -i 's/- vim\n    action: install/- vim\n    - curl\n    - wget\n    - bash\n    - lsof\n    - sshpass\n    - openssh-server\n    - iptables\n    - dos2unix\n    action: install/g' debian.yaml
 insert_content=$(cat bash_insert_content.text)
-sed -i -e "/mappings:/i $insert_content" debian.yaml
+sed -i -e "/mappings:/i $(echo "$insert_content" | sed 's/\//\\\//g')" debian.yaml

aba59bb42f004223983007240259af5fb3d3cf7d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-01-13 22:06:23 +0800

    Update build_images.sh

--- build_images.sh
+++ build_images.sh
@@ -2,25 +2,35 @@
 # from https://github.com/oneclickvirt/incus_images
 
 
-sudo apt-get install zip -y
-sudo snap install distrobuilder --classic
-sudo apt-get install debootstrap -y
-debian_versions=("buster" "bullseye" "bookworm" "trixie")
-debian_ver_nums=("10" "11" "12" "13")
-architectures=("amd64") # "arm64" "armel" "armhf" "i386" "ppc64el" "s390x"
-variants=("default" "cloud")
-len=${#debian_versions[@]}
-for ((i=0; i<len; i++)); do
-    version=${debian_versions[i]}
-    ver_num=${debian_ver_nums[i]}
-    for arch in "${architectures[@]}"; do
-        for variant in "${variants[@]}"; do
-            sudo distrobuilder build-incus /home/runner/work/incus_images/incus_images/images_yaml/debian.yaml -o image.release=${version} -o image.architecture=${arch} -o image.variant=${variant}
-            if [ -f incus.tar.xz ] && [ -f rootfs.squashfs ]; then
-                zip debian_${ver_num}_${version}_${arch}_${variant}.zip incus.tar.xz rootfs.squashfs
-                rm -rf incus.tar.xz rootfs.squashfs
-            fi
+opath=$(pwd)
+if ! command -v zip >/dev/null 2>&1; then
+    sudo apt-get install zip -y
+fi
+if ! command -v distrobuilder >/dev/null 2>&1; then
+    sudo snap install distrobuilder --classic
+fi
+if ! command -v debootstrap >/dev/null 2>&1; then
+    sudo apt-get install debootstrap -y
+fi
+
+debian(){
+    # debian
+    debian_versions=("buster" "bullseye" "bookworm" "trixie")
+    debian_ver_nums=("10" "11" "12" "13")
+    architectures=("amd64" "arm64") # "armel" "armhf" "i386" "ppc64el" "s390x"
+    variants=("default" "cloud")
+    len=${#debian_versions[@]}
+    for ((i=0; i<len; i++)); do
+        version=${debian_versions[i]}
+        ver_num=${debian_ver_nums[i]}
+        for arch in "${architectures[@]}"; do
+            for variant in "${variants[@]}"; do
+                sudo distrobuilder build-incus ${opath}/images_yaml/debian.yaml -o image.release=${version} -o image.architecture=${arch} -o image.variant=${variant}
+                if [ -f incus.tar.xz ] && [ -f rootfs.squashfs ]; then
+                    zip debian_${ver_num}_${version}_${arch}_${variant}.zip incus.tar.xz rootfs.squashfs
+                    rm -rf incus.tar.xz rootfs.squashfs
+                fi
+            done
         done
     done
-done
-cd /home/runner/work/incus_images/incus_images
+}

49d015db9827df8b458a026eeb9c85bf0b664a62
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-01-13 17:41:17 +0800

    Update clone_modify_yaml.sh

--- clone_modify_yaml.sh
+++ clone_modify_yaml.sh
@@ -2,10 +2,11 @@
 
 wget https://raw.githubusercontent.com/lxc/lxc-ci/main/images/debian.yaml
 chmod 777 debian.yaml
-sed -i 's/- vim\n    action: install/- vim\n    - curl\n    - wget\n    - bash\n    - lsof\n    - sshpass\n    - openssh-server\n    - iptables\n    - dos2unix\n    action: install/g' debian.yaml
-insert_content=$(cat bash_insert_content.text)
+insert_content_1="    - curl\n    - wget\n    - bash\n    - lsof\n    - sshpass\n    - openssh-server\n    - iptables\n    - dos2unix"
+sed -i "/- vim/ a\\$insert_content_1" debian.yaml
+insert_content_2=$(cat bash_insert_content.text)
 line_number=$(($(wc -l < debian.yaml) - 2))
 head -n $line_number debian.yaml > temp.yaml
-echo "$insert_content" >> temp.yaml
+echo "$insert_content_2" >> temp.yaml
 tail -n 2 debian.yaml >> temp.yaml
 mv temp.yaml debian.yaml
